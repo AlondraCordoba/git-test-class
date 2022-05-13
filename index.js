6e5172d0812d7f0ba204d436d015cede282dbe8a
AlondraCordoba <alondracordoba0720@gmail.com>
2022-05-12 19:56:23 -0500

    console.log added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function doSomething() {
 }
 function develop() {
     console.log("I'm in developing");
+    console.log("I'm developing a new project :)");
 }
 function testing() {
     console.log("testing");
